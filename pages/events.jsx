77aaee1e9a95b98a838b685d38014bde46fbc37c
Sarah Donoghue <sarahdonoghue@Peters-iMac-452.local>
2019-08-09 14:54:40 +0100

    Style events page layout

--- pages/events.jsx
+++ pages/events.jsx
@@ -2,6 +2,16 @@ import exact from 'prop-types-exact';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+
+const propTypes = {
+  title: PropTypes.string,
+}
+
+const defaultProps = {
+  title: 'Find an event...'
+}
 
 class Events extends Component {
   constructor(props) {
@@ -9,10 +19,28 @@ class Events extends Component {
   }
 
   render() {
+    const { title } = this.props;
+
     return(
-      <h1>Events Page</h1>
+      <React.Fragment>
+        <div className="Events">
+        <Navigation />
+          <div className="Events_bannerContainer">
+            <div className="Events_heroImage" />
+            <div className="Events_content">
+              <h1 className="Events_bannerTitle">
+                {title}
+              </h1>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </React.Fragment>
     )
   }
 }
+
+Events.propTypes = propTypes;
+Events.defaultProps = defaultProps;
 
 export default Events;
